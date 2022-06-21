07933bccb516be379bd00ae1b5c76d4afa4e8ec3
afdezcl <adrifdez_@uma.es>
2022-06-21 14:27:58 +0200

    Refactor custom hook useExtension

--- frontend-gitextension/src/hooks/useExtension.tsx
+++ frontend-gitextension/src/hooks/useExtension.tsx
@@ -1,33 +1,18 @@
 import { useState, useEffect } from 'react'
-import { GitHubTree, GitHubTreesResponse } from '../models/github-responses/treesReponse.interface'
 import { getExtensions } from '../services/getExtension'
 
-export default function useExtensions(user: string, repo: string) {
+export default function useExtensions(user: string, repo: string): { 
+  loading: boolean, 
+  extensions: Record<string, number> | null, 
+  error: boolean 
+} {
   const [loading, setLoading] = useState<boolean>(false)
   const [error, setError] = useState<boolean>(false)
   const [extensions, setExtensions] = useState<Record<string, number>>({})
 
-  const filterFilesWithoutExtensions = (paths: string[]): string[] => {
-    const files = paths.filter((item: string) => item.includes('.'))
-    return files
-  }
-
-  const createObjectOfExtensions = (files: string[]): Record<string, number> => {
-    return files.reduce((acc: any, curr: string) => {
-      const indexDoc = curr.indexOf(".")
-      const file = curr.substring(indexDoc + 1, curr.length)
-      return acc[file] ? ++acc[file] : (acc[file] = 1), acc
-    }, {})
-  }
-
   useEffect(() => {
     setLoading(true)
-    getExtensions(user, repo).then((response: GitHubTreesResponse) => {
-      const paths = response.tree.map((item: GitHubTree) => {
-        return item.path
-      })
-      const files = filterFilesWithoutExtensions(paths)
-      const extensionsList = createObjectOfExtensions(files)
+    getExtensions(user, repo).then((extensionsList: Record<string, number>) => {
       setExtensions(extensionsList)
       setError(false)
     }).catch(() => {
