d6e0e31338e4fc090317dce012c30f84d74ab3fb
afdezcl <adrifdez_@uma.es>
2022-06-21 14:28:34 +0200

    Delegate calculates to getExtension service

--- frontend-gitextension/src/services/getExtension.tsx
+++ frontend-gitextension/src/services/getExtension.tsx
@@ -1,8 +1,26 @@
 import { CommitsResponse } from "../models/github-responses/commitsResponse.interface"
+import { GitHubTree, GitHubTreesResponse } from "../models/github-responses/treesReponse.interface"
+
+const calculateFilesOcurrences = (files: string[]): Record<string, number> => {
+  return files.reduce((acc: any, curr: string) => {
+    if(!curr.includes('.')) return {...acc}
+    const indexDoc = curr.indexOf(".")
+    const fileName = curr.substring(indexDoc + 1, curr.length)
+    return acc[fileName] ? ++acc[fileName] : (acc[fileName] = 1), acc
+  }, {})
+} 
+
+const fromApiResponseToExtensions = (apiResponse: GitHubTreesResponse) => {
+  const files = apiResponse.tree.map((item: GitHubTree) => {
+    return item.path
+  })
+  const extensionsList = calculateFilesOcurrences(files)
+  return extensionsList
+}
 
 export const getExtensions = (user: string, repo: string) => {
   const API = 'https://api.github.com/repos/'
-    
+  
   return fetch(
     `${API}${user}/${repo}/commits`
   )
@@ -14,4 +32,5 @@ export const getExtensions = (user: string, repo: string) => {
       )
     })
     .then((response) => response.json())
+    .then(fromApiResponseToExtensions)
 }
